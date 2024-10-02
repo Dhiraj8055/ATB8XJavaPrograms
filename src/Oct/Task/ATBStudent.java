9dfb949f4255482e4fb7220c61d0ebb536df0fb1
Dhiraj8055 <holkardhiraj@rediffmail.com>
2024-10-02 21:37:15 +0530

    Student Class

---
+++ src/Oct/Task/ATBStudent.java
@@ -0,0 +1,16 @@
+package Oct.Task;
+
+public class ATBStudent {
+
+    public static void main (String [] args) {
+        int roll_no, age;
+        char firstname, lastname, std, address, DOB, email, parent_name;
+        long contact_no;
+
+        ATBStudent getroll_no = new ATBStudent();
+        ATBStudent getname = new ATBStudent();
+        ATBStudent getaddress = new ATBStudent();
+        ATBStudent get_age = new ATBStudent();
+        ATBStudent get_contact_no = new ATBStudent();
+    }
+}
--- src/Sept/ex_06092024/Task001.java
+++ src/Sept/ex_06092024/Task001.java
@@ -1,4 +1,5 @@
 package Sept.ex_06092024;
 
 public class Task001 {
+    //System.out.println("Hello");
 }
